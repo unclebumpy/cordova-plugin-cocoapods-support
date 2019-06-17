b159d97e4aa99e0eddb4de2bbe818bfa5b39af5b
James S <unclebumpy@users.noreply.github.com>
2019-06-17 15:02:59 -0700

    Change xml2js path back

--- scripts/podify.js
+++ scripts/podify.js
@@ -3,7 +3,7 @@
 var _ = require('lodash');
 var fs = require('fs');
 var path = require("path");
-var xml2js = require('./lib/xml2js');
+var xml2js = require('xml2js');
 var spawn = require('child_process').spawn;
 var parser = new xml2js.Parser();
 require('shelljs/global');
